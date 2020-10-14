a0a8bc45447b53bf5c9a50eeb58dbaccfd5b8f25
Швыкин Семен <Sshvykin@innotehnum.com>
2020-10-14 22:23:25 +0300

    All Request Models

---
+++ src/main/java/dto/ViewModels/Request/VmPlanSectionRequest.java
@@ -0,0 +1,35 @@
+package dto.ViewModels.Request;
+
+import org.springframework.lang.Nullable;
+
+import java.util.Calendar;
+
+public class VmPlanSectionRequest {
+    @Nullable
+    private Calendar startDate;
+    @Nullable
+    private Calendar finishDate;
+
+    public VmPlanSectionRequest(@Nullable Calendar startDate, @Nullable Calendar finishDate) {
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+    }
+
+    @Nullable
+    public Calendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(@Nullable Calendar startDate) {
+        this.startDate = startDate;
+    }
+
+    @Nullable
+    public Calendar getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(@Nullable Calendar finishDate) {
+        this.finishDate = finishDate;
+    }
+}
---
+++ src/main/java/dto/ViewModels/Request/VmSystemsRequest.java
@@ -0,0 +1,33 @@
+package dto.ViewModels.Request;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+
+public class VmSystemsRequest extends VmPageRequestBase {
+    @Nullable
+    private Boolean onlyMine;
+    private String keyword;
+    @Autowired
+    public VmSystemsRequest(@Nullable Boolean onlyMine, String keyword, int limit, int page) {
+        super(limit, page);
+        this.onlyMine = onlyMine;
+        this.keyword = keyword;
+    }
+
+    @Nullable
+    public Boolean getOnlyMine() {
+        return onlyMine;
+    }
+
+    public void setOnlyMine(@Nullable Boolean onlyMine) {
+        this.onlyMine = onlyMine;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
