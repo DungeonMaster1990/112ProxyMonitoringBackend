ebdaec14adb29eb255d6803e6731c429505ae2e7
IVShvykin <IVShvykin@gmail.com>
2020-10-15 12:39:52 +0300

    fix

--- src/main/java/Controllers/PlansController.java
+++ src/main/java/Controllers/PlansController.java
@@ -1,20 +1,33 @@
 package Controllers;
 
 import Mock.VmMock.VmMock;
+import dto.ViewModels.Request.VmPlanRequest;
 import dto.ViewModels.Request.VmPlanSectionRequest;
+import dto.ViewModels.Response.VmPlanInfoResponse;
+import dto.ViewModels.Response.VmPlanResponse;
 import dto.ViewModels.Response.VmPlanSectionsResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v{1.0}/plans")
 public class PlansController {
 
     @PostMapping(value = "/sections", consumes = "application/json", produces = "application/json")
-    public VmPlanSectionsResponse[] GetPlanSections(@RequestBody VmPlanSectionRequest vmPlanSectionRequest)
+    public VmPlanSectionsResponse[] getPlanSections(@RequestBody VmPlanSectionRequest vmPlanSectionRequest)
     {
         return VmMock.vmPlanSectionsResponse;
+    }
+
+    @PostMapping("")
+    public VmPlanResponse[] get(@RequestBody VmPlanRequest vmPlanRequest)
+    {
+        return VmMock.vmPlanResponse;
+    }
+
+    @GetMapping("/info/{id}")
+    public VmPlanResponse[] getPlanInfo(@PathVariable String id)
+    {
+        return  VmMock.vmPlanResponse;
     }
 }
--- src/main/java/Mock/VmMock/VmMock.java
+++ src/main/java/Mock/VmMock/VmMock.java
@@ -1,11 +1,23 @@
 package Mock.VmMock;
 
+import dto.ViewModels.Enums.BlAccidentStatusType;
+import dto.ViewModels.Response.VmPlanResponse;
 import dto.ViewModels.Response.VmPlanSectionsResponse;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class VmMock {
     public static VmPlanSectionsResponse[] vmPlanSectionsResponse =
             new VmPlanSectionsResponse[]
     {
         new VmPlanSectionsResponse("1", "Экстренные", 1), new VmPlanSectionsResponse("2", "Плановые", 5)
+    };
+
+    public static VmPlanResponse[] vmPlanResponse =
+            new VmPlanResponse[]
+    {
+        new VmPlanResponse("1", "Изменение IM-283501", "Согласование", BlAccidentStatusType.warning, "Описание", new String[]{"Платежи", "Переводы"}, new GregorianCalendar(2020, Calendar.AUGUST, 2)),
+        new VmPlanResponse("2", "Изменение IM-283501", "Выполнено", BlAccidentStatusType.normal, "Описание", new String[]{ "Платежи" }, new GregorianCalendar(2016, Calendar.JULY, 5))
     };
 }
