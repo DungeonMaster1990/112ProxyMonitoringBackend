915da066e231f6c4cbea51f14709297d8295d3ce
IVShvykin <IVShvykin@gmail.com>
2020-10-15 20:36:05 +0300

    metrics fix

--- src/main/java/Controllers/MetricsController.java
+++ src/main/java/Controllers/MetricsController.java
@@ -1,8 +1,11 @@
 package Controllers;
 
 import Mock.VmMock.VmMock;
+import dto.ViewModels.Request.VmMetricInfoRequest;
 import dto.ViewModels.Request.VmMetricsRequest;
+import dto.ViewModels.Response.VmMetricInfoResponse;
 import dto.ViewModels.Response.VmMetricsResponse;
+import dto.ViewModels.Response.VmUpdateResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +19,17 @@ public class MetricsController {
     public VmMetricsResponse[] get(@RequestBody VmMetricsRequest vmMetricsRequest)
     {
         return VmMock.vmMetricsResponse;
+    }
+
+    @PostMapping(value = "/update", consumes = "application/json", produces = "application/json")
+    public VmUpdateResponse updateMetrics(@RequestBody String[] vmUpdateRequest)
+    {
+        return VmMock.updateMetricsOrSystem
+    }
+
+    @PostMapping(value = "/info", consumes = "application/json", produces = "application/json")
+    public VmMetricInfoResponse[] getMetricInfo(@RequestBody VmMetricInfoRequest vmMetricInfoRequest)
+    {
+        return VmMock.vmMetricInfoResponse;
     }
 }
