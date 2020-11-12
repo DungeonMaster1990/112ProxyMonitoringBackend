9e3c5b514388f3318478a734d9c9b281bd65e1bd
IVShvykin <IVShvykin@gmail.com>
2020-11-12 12:31:02 +0300

    vertica migration

--- Database/Migrations/Vertica.sql
+++ Database/Migrations/Vertica.sql
@@ -1,132 +1,46 @@
-create schema if not exists monitoring;
-
-create table if not exists  monitoring.accidentStatuses
+create sequence if not exists monitoring.sm_rawdata_meas_id_seq;
+create table if not exists monitoring.sm_rawdata_meas
 (
-    id   serial primary key,
-    name varchar
-);
-create table if not exists monitoring.failureStatuses
+    id                         integer not null default nextval('monitoring.sm_rawdata_meas_id_seq'),
+    session_id                 int not null,
+    measurement_id             int not null,
+    sched_id                   int not null,
+    category_id                int not null,
+    monitor_id                 int,
+    target_id                  int not null,
+    msname                     varchar not null,
+    msid                       int,
+    userRemark                 varchar,
+    connectionData             varchar,
+    dmConnectionId             int,
+    active                     int,
+    ciId                       varchar,
+    etiId                      varchar,
+    integrationName            varchar,
+    profileId                  varchar,
+    modified_date              timestamp,
+    creation_date              timestamp,
+    is_deleted                 boolean
+    );
+alter sequence monitoring.sm_rawdata_meas_id_seq
+owned by monitoring.sm_rawdata_meas.id;
+
+create sequence if not exists monitoring.sm_def_measurements_id_seq;
+create table if not exists monitoring.sm_def_measurements
 (
-    id         serial primary key,
-    name       varchar,
-    accidentId int
+    id                         integer not null default nextval('monitoring.sm_def_measurements_id_seq'),
+    session_id                 int not null,
+    time_stamp                 timestamp not null,
+    measurement_id             int not null,
+    meas_value                 numeric,
+    status_id                  int,
+    errMsg                     varchar,
+    raw_monitor_id             int not null,
+    raw_target_id              int not null,
+    raw_connection_id          int not null,
+    raw_category_id            int not null,
+    raw_threshold_quality      int,
+    dbdate                     timestamp
 );
-
-create sequence if not exists monitoring.systems_id_seq;
-create table if not exists monitoring.systems
-(
-    id         integer not null default nextval('monitoring.systems_id_seq'),
-    name       varchar,
-    accidentId int
-);
-alter sequence monitoring.systems_id_seq
-owned by monitoring.systems.id;
-
-create table if not exists monitoring.plantypes
-(
-    id   serial primary key,
-    name varchar
-);
-
-create sequence if not exists monitoring.plans_id_seq;
-create table if not exists monitoring.plans
-(
-    id                         integer not null default nextval('monitoring.plans_id_seq'),
-    foreignId                  varchar not null,
-    plantypeId                 int     not null,
-    phase                      int,
-    status                     varchar,
-    shortDescription           varchar,
-    impactDescription          varchar,
-    degradationRate            varchar,
-    startDate                  timestamp,
-    finishDate                 timestamp,
-    startDownDate              timestamp,
-    finishDownDate             timestamp,
-    initiator                  varchar,
-    customer                   varchar,
-    owner                      varchar,
-    implementationPlan         varchar,
-    rollbackPlan               varchar,
-    itSystems                  varchar,
-    configurationUnits         varchar,
-    affectedConfigurationUnits varchar,
-    affectedItSystems          varchar,
-    affectedItServices         varchar
-);
-alter sequence monitoring.plans_id_seq
-owned by monitoring.plans.id;
-
-create sequence if not exists monitoring.incidents_id_seq;
-create table if not exists monitoring.incidents
-(
-    id                                   integer not null default nextval('monitoring.incidents_id_seq'),
-    incident_id                          varchar not null,
-    created_at                           timestamp,
-    expired_at                           timestamp,
-    author_id                            int,
-    contact_id                           int,
-    service_id                           int,
-    category                             varchar,
-    failure_point                        varchar,
-    configuration_item_id                int,
-    title                                varchar,
-    description                          varchar,
-    impact                               varchar,
-    severity                             varchar,
-    group_id                             int,
-    assignee_id                          varchar,
-    status                               varchar,
-    close_code                           varchar,
-    resolution                           varchar,
-    comment                              varchar,
-    updated_at                           timestamp,
-    type                                 varchar,
-    sla_start_time                       timestamp,
-    jira_number                          varchar,
-    template_name                        varchar,
-    ext_organization                     varchar,
-    ext_id                               varchar,
-    ext_status                           varchar,
-    ext_assignee_time                    varchar,
-    source                               varchar,
-    specialist_id                        varchar,
-    priority                             varchar,
-    idented_at                           timestamp,
-    fact_end_at                          timestamp,
-    fact_begin_at                        timestamp
-);
-
-alter sequence monitoring.incidents_id_seq
-owned by monitoring.incidents.id;
-
-create sequence if not exists monitoring.unavailability_id_seq;
-create table monitoring.unavailability
-(
-    id              integer not null default nextval('monitoring.unavailability_id_seq'),
-    fault_id        varchar,
-    begin_at        timestamp,
-    end_at          timestamp,
-    duration        interval,
-    service_name    varchar,
-    type            varchar,
-    service_id      varchar,
-    created_at      varchar,
-    created_by_id   varchar,
-    updated_at      timestamp,
-    updated_by_id   varchar
-);
-
-alter sequence monitoring.unavailability_id_seq
-owned by monitoring.unavailability.id;
-
-create sequence if not exists monitoring.pushTokens_id_seq;
-create table monitoring.pushTokens
-(
-    id         integer not null default nextval('monitoring.pushTokens_id_seq'),
-    token      varchar,
-    install_id varchar,
-    platform   varchar
-);
-alter sequence monitoring.pushTokens_id_seq
-owned by monitoring.pushTokens.id;
+alter sequence monitoring.sm_def_measurements_id_seq
+owned by monitoring.sm_def_measurements.id;
