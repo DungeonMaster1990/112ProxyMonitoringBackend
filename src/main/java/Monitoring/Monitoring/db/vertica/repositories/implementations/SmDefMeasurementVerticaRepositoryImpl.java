9085094d9795815fadf916549cca8ce62c7cacf9
IVShvykin <IVShvykin@gmail.com>
2020-11-17 14:25:47 +0300

    query fix

--- src/main/java/Monitoring/Monitoring/db/vertica/repositories/implementations/SmDefMeasurementVerticaRepositoryImpl.java
+++ src/main/java/Monitoring/Monitoring/db/vertica/repositories/implementations/SmDefMeasurementVerticaRepositoryImpl.java
@@ -31,9 +31,10 @@ public class SmDefMeasurementVerticaRepositoryImpl implements SmDefMeasurementVe
         List<SmDefMeasurementVertica> smDefMeasurementsVertica = new ArrayList<SmDefMeasurementVertica>();
         Statement stmt = verticaConnection.createStatement();
         String query = String.format("""
-                    select from bsm_replica.SM_DEF_MEASUREMENT session_id, measurement_id, shed_id, category_id, monitor_id, target_id,
+                    select session_id, measurement_id, shed_id, category_id, monitor_id, target_id,
                     msname, msid, user_remark, connection_data, dm_connection_id, active, ci_id, eti_id, integration_name,
-                    profile_id, creation_date, modified_date, deleted
+                    profile_id, creation_date, modified_date, deleted 
+                    from bsm_replica.SM_DEF_MEASUREMENT
                     where measurement_id in (%s)
                 """, metrics
         );
