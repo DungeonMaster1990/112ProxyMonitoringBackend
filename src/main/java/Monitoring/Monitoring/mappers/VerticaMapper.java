0e7772b65c6beec85a5636da743cc45230b632b6
IVShvykin <IVShvykin@gmail.com>
2020-11-18 11:37:11 +0300

    fix

--- src/main/java/Monitoring/Monitoring/mappers/VerticaMapper.java
+++ src/main/java/Monitoring/Monitoring/mappers/VerticaMapper.java
@@ -4,7 +4,9 @@ import Monitoring.Monitoring.db.models.SmDefMeasurementApi;
 import Monitoring.Monitoring.db.models.SmRawdataMeasApi;
 import Monitoring.Monitoring.db.vertica.models.SmDefMeasurementVertica;
 import Monitoring.Monitoring.db.vertica.models.SmRawdataMeasVertica;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface VerticaMapper {
    SmRawdataMeasApi mapToSmRawdataMeasApi(SmRawdataMeasVertica metrics);
    SmDefMeasurementApi mapToSmDefMeasurementApi(SmDefMeasurementVertica metrics);
