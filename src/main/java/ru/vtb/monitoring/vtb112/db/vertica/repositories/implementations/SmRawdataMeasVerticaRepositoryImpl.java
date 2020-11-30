7df643efacaf7026415f3d566b9d0bf5254302a2
IVShvykin <IVShvykin@gmail.com>
2020-11-30 22:07:10 +0300

    vertica measurements paging

--- src/main/java/ru/vtb/monitoring/vtb112/db/vertica/repositories/implementations/SmRawdataMeasVerticaRepositoryImpl.java
+++ src/main/java/ru/vtb/monitoring/vtb112/db/vertica/repositories/implementations/SmRawdataMeasVerticaRepositoryImpl.java
@@ -33,30 +33,42 @@ public class SmRawdataMeasVerticaRepositoryImpl implements SmRawdataMeasVerticaR
 
         Statement stmt = verticaConnection.getConnection().createStatement();
         Timestamp timestamp = Timestamp.from(lastUpdate.getUpdateTime().toInstant());
-        String query = String.format("""
+        Integer offset = 0;
+        String query = """
                     select session_id, time_stamp, measurement_id, status_id, err_msg, raw_monitor_id, raw_target_id, 
                     raw_connection_id, raw_category_id, raw_threshold_quality, dbdate, meas_value
                     from bsm_replica.SM_RAWDATA_MEAS
                     where status_id = 0 and time_stamp > '%s'
-                """, new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(timestamp));
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-            SmRawdataMeasVertica smRawdataMeasVertica = new SmRawdataMeasVertica();
+                    limit 100
+                    offset %s
+                """;
+        while (true) {
+            String newQuery = String.format(query, new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(timestamp), Integer.toString(offset));
+            ResultSet rs = stmt.executeQuery(newQuery);
+            offset = offset + 100;
+            if (!rs.next()) {
+                break;
+            } else {
+                do {
+                    SmRawdataMeasVertica smRawdataMeasVertica = new SmRawdataMeasVertica();
 
-            smRawdataMeasVertica.setSessionId(rs.getInt("session_id"));
-            smRawdataMeasVertica.setTimeStamp(dateFormatterHelper.dbDateToZonedDate(rs.getTimestamp("time_stamp")));
-            smRawdataMeasVertica.setMeasurementId(rs.getInt("measurement_id"));
-            smRawdataMeasVertica.setMeasValue(rs.getFloat("meas_value"));
-            smRawdataMeasVertica.setStatusId(rs.getInt("status_id"));
-            smRawdataMeasVertica.setErrMsg(rs.getString("err_msg"));
-            smRawdataMeasVertica.setRawMonitorId(rs.getInt("raw_monitor_id"));
-            smRawdataMeasVertica.setRawTargetId(rs.getInt("raw_target_id"));
-            smRawdataMeasVertica.setRawConnectionId(rs.getInt("raw_connection_id"));
-            smRawdataMeasVertica.setRawCategoryId(rs.getInt("raw_category_id"));
-            smRawdataMeasVertica.setRawThresholdQuality(rs.getInt("raw_threshold_quality"));
-            smRawdataMeasVertica.setDbdate(dateFormatterHelper.dbDateToZonedDate(rs.getTimestamp("dbdate")));
+                    smRawdataMeasVertica.setSessionId(rs.getInt("session_id"));
+                    smRawdataMeasVertica.setTimeStamp(dateFormatterHelper.dbDateToZonedDate(rs.getTimestamp("time_stamp")));
+                    smRawdataMeasVertica.setMeasurementId(rs.getInt("measurement_id"));
+                    smRawdataMeasVertica.setMeasValue(rs.getFloat("meas_value"));
+                    smRawdataMeasVertica.setStatusId(rs.getInt("status_id"));
+                    smRawdataMeasVertica.setErrMsg(rs.getString("err_msg"));
+                    smRawdataMeasVertica.setRawMonitorId(rs.getInt("raw_monitor_id"));
+                    smRawdataMeasVertica.setRawTargetId(rs.getInt("raw_target_id"));
+                    smRawdataMeasVertica.setRawConnectionId(rs.getInt("raw_connection_id"));
+                    smRawdataMeasVertica.setRawCategoryId(rs.getInt("raw_category_id"));
+                    smRawdataMeasVertica.setRawThresholdQuality(rs.getInt("raw_threshold_quality"));
+                    smRawdataMeasVertica.setDbdate(dateFormatterHelper.dbDateToZonedDate(rs.getTimestamp("dbdate")));
 
-            smRawdataMeasesVertica.add(smRawdataMeasVertica);
+                    smRawdataMeasesVertica.add(smRawdataMeasVertica);
+                } while (rs.next());
+            }
+
         }
         return smRawdataMeasesVertica;
     }
