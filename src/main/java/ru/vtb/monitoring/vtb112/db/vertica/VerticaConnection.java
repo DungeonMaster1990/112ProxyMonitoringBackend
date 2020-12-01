563c2570e42a811bee7ce322d39f19e747c01dbf
Alexander Stepchkov <stepchkov@gmail.com>
2020-12-01 10:29:28 +0300

    Closed connection for vertica

--- src/main/java/ru/vtb/monitoring/vtb112/db/vertica/VerticaConnection.java
+++ src/main/java/ru/vtb/monitoring/vtb112/db/vertica/VerticaConnection.java
@@ -1,29 +1,35 @@
 package ru.vtb.monitoring.vtb112.db.vertica;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.vtb.monitoring.vtb112.config.AppConfig;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @Component
-public class VerticaConnection {
+public class VerticaConnection implements AutoCloseable {
     private Connection connection;
-    private final AppConfig appConfig;
+    private final String verticaUrl;
+    private final Properties verticaUserPass;
 
-    @Autowired
     public VerticaConnection(AppConfig appConfig) {
-        this.appConfig = appConfig;
+        this.verticaUrl = appConfig.getVerticaUrl();
+        this.verticaUserPass = appConfig.getVerticaUserPass();
     }
 
     public Connection getConnection() throws SQLException {
-        if(this.connection == null) {
-            this.connection = DriverManager
-                    .getConnection(appConfig.getVerticaUrl(), appConfig.getVerticaUserPass());
-
+        if (this.connection == null || this.connection.isClosed()) {
+            this.connection = DriverManager.getConnection(verticaUrl, verticaUserPass);
         }
         return this.connection;
+    }
+
+    @Override
+    public void close() throws SQLException {
+        if (this.connection != null && !this.connection.isClosed()) {
+            this.connection.close();
+        }
     }
 }
